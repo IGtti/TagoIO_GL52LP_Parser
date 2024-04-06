79232dc4883e27ff29585a025e16f3c54eb1ef65
Iaran Gadotti <gadotti@hotmail.com>
2024-04-06 10:44:01 -0300

    New Messages and Buffer size
    Message type +RSP:GTCTN:, +RSP:GTSTR:, +EVT:GTNMR:, +EVT:GTRTL:

--- GL52LPDeviceParser.js
+++ GL52LPDeviceParser.js
@@ -15,24 +15,26 @@ if (lora_payload) {
   // convert the data from Hex to Javascript Buffer
   const lora_buffer = Buffer.from(lora_payload.value, 'hex');                   // read string in bytes
   const FixType = (lora_buffer[9] & 0x60) >> 5;                                 // <Fix Type>: Bit5-Bit6 of the 4th byte indicate Fix Type
+  const bsize = lora_buffer.byteLength;                                         // buffer size in bytes --> 17 for GNSS location and battery only. Not considering UTC, Altitude, Azimuth and Speed
   let longitude;                                                                // Longitude
   let latitude;                                                                 // Latitude
   let battery;                                                                  // Battery Percentage
 
   payload = [];                                                                 // clean the payload array
 
-  if (lora_buffer[0] == 0x01) {                                                 // Message type +RSP:GTCTN: Location report in continuous mode
+  if (((lora_buffer[0] == 0x01) || (lora_buffer[0] == 0x09) || (lora_buffer[0] == 0x02) || (lora_buffer[0] == 0x1A)) && (bsize == 17)) {       // Message type +RSP:GTCTN:, +RSP:GTSTR:, +EVT:GTNMR:, +EVT:GTRTL:
     if ((FixType == 1) || (FixType == 2)) {                                     // <Fix Type>: 1 for 2D or 2 for 3D
       longitude = ((lora_buffer[5] & 0xFF) << 24) + ((lora_buffer[6] & 0xFF) << 16) + ((lora_buffer[7] & 0xFF) << 8) + lora_buffer[8];         // Convert 4 bytes to signed integer
       longitude = longitude / 1000000;                                          // 6 implicit decimals
 
-      latitude = ((lora_buffer[9] & 0x0F) << 24) + ((lora_buffer[10] & 0xFF) << 16) + ((lora_buffer[11] & 0xFF) << 8) + lora_buffer[12];         // Convert 3.5 bytes to integer
+      latitude = ((lora_buffer[9] & 0x0F) << 24) + ((lora_buffer[10] & 0xFF) << 16) + ((lora_buffer[11] & 0xFF) << 8) + lora_buffer[12];       // Convert 3.5 bytes to integer
       latitude = latitude / 1000000;                                            // 6 implicit decimals
       if((lora_buffer[9] & 0x10) == 0x10) latitude = latitude * -1;             // Check if latitude is negative
-    }
 
-    // Building the TagoIO payload format
-    payload.push({"variable": "tracker_location", "value": latitude, "location": {"lat": latitude, "lng": longitude}});              // build the decoded payload
+
+      // Building the TagoIO payload format
+      payload.push({"variable": "tracker_location", "value": latitude, "location": {"lat": latitude, "lng": longitude}});              // build the decoded payload
+    }
 
     battery = lora_buffer[13];
     //console.log(battery);
